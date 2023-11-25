8bd9d67cb85d20fff7ed16ea726c49dc3e8dee99
j-cmar <jasonmar2004.2@gmail.com>
2023-11-24 22:14:11 -0800

    adding build

---
+++ src/pages/API.js
@@ -0,0 +1,5 @@
+const API = () => {
+	return <h1>api consumption</h1>;
+};
+
+export default API;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+	return (
+		<>
+			<h1>Welcome to my Home Page</h1>
+			<p>
+				Please click the above links to see the rest of the assignment
+			</p>
+		</>
+	);
+};
+
+export default Home;
